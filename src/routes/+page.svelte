a6487778bca1b8d2fdc212ed188ee3afaa305605
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-11-13 15:24:49 +0200

    add logo and play button to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Button from '../lib/components/Button.svelte';
+</script>
+
+<main class="flex flex-col w-full mt-8">
+    <div class="flex content-center justify-center">
+        <img class="w-[60vw] drop-shadow-[0px_10px_0px_rgba(231,_101,_49,_0.15)]" src="assets/svg/logo.svg" alt="Logo"/>
+    </div>
+    <div class="flex flex-row justify-start">
+        <div class="w-1/3 p-8">
+            <div class="m-24">
+                <Button 
+                color="orange" 
+                bgcolor="orangeDark" 
+                w="10" 
+                h="10" 
+                m="24" 
+                svg="assets/svg/play.svg"
+            />
+            </div>
+        </div>
+        <div class="w-2/3">
+            car
+        </div>
+    </div>
+</main>
