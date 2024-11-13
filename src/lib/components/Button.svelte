51f223b8c18340f0045135341a2f78ca2d474720
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-11-13 15:21:44 +0200

    add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { color, bgcolor, w, h, m, svg } = $props();
+</script>
+
+<div class="flex flex-row items-center">
+    <button class="bg-{color} rounded-lg z-10 active:translate-y-1 ease-out duration-300">
+        <img class="w-{w} m-{m}" src="{svg}" alt="Back arrow"/>
+    </button>
+    <div class="bg-{bgcolor} rounded-lg absolute translate-y-1">
+        <div class="w-{w} h-{h} m-{m}"></div>
+    </div>
+</div>
