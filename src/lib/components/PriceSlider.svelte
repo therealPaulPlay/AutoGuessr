2abc89779126373b92fc56de98a74a733436e8a4
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-11-28 15:15:34 +0200

    allow user to input price number using keys

--- src/lib/components/PriceSlider.svelte
+++ src/lib/components/PriceSlider.svelte
@@ -1,24 +1,42 @@
 <script>
+
     let {
         sliderMin=1,
         sliderMax=500_000,
         children
     } = $props();
 
-    let guess = $state(0);
+    const clamp = (val, min, max) => Math.min(Math.max(val, min), max)
+
+    let guess = $state(sliderMin);
+    let restrictedGuess = $derived(clamp(guess, sliderMin, sliderMax));
 
     function onGuessChange() {
         
     }
 
+    function formatedGuess() {
+        return '$' + restrictedGuess.toLocaleString();
+    }
+
+    function preventNonNumericalInput(e) {
+        e = e || window;
+        var charCode = (typeof e.which == "undefined") ? e.keyCode : e.which;
+        var charStr = String.fromCharCode(charCode);
+
+        if (!charStr.match(/^[0-9]+$/))
+            e.preventDefault();
+    }
+
 </script>
 
-<div class="flex flex-row items-center h-[4.2rem] rounded-lg bg-tanLight p-2 drop-shadow-[0_0.3rem_0_var(--white-shadow)]">
+<div class="flex flex-row items-center h-[4.2rem] rounded-lg bg-tanLight p-2 drop-shadow-[0_0.3rem_0_var(--white-shadow)] ">
     <div>
         <input type="range" step="1" min={sliderMin} max={sliderMax} bind:value={guess} onchange={onGuessChange} class="slider" />
     </div>
-    <div class="flex items-center justify-center bg-white text-xl p-2 px-4 rounded-md h-full ml-2 text-orange font-bold min-w-32">
-        <input type="number" bind:value={guess} class="w-full h-full bg-transparent text-center" />
+    <div class="flex items-center justify-center bg-white text-xl p-2 px-4 rounded-md h-full ml-2 text-orange font-bold min-w-16 relative">
+        <span class="w-full h-full bg-transparent text-center">{formatedGuess()}</span>
+        <input type="number" bind:value={guess} onchange={formatedGuess()} onkeypress={preventNonNumericalInput} class="absolute w-full h-full" style="opacity: 0%;"/>
     </div>
 </div>
 
@@ -52,11 +70,18 @@
         cursor: pointer;
     }
 
-    /*  TODO:   Make the input arrows disappear on Chrome too. 
-                Figure out why chrome shows slider thinner. 
-                Make input outline disappear. 
+    /*  TODO:   Figure out why chrome shows slider thinner. 
     */
 
+    /* Removes number arrows */
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
     input[type=number] {
         -moz-appearance: textfield;
         appearance: textfield;
