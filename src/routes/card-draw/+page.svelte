94121e30dfcd3f54992181534844e07f6def1ad3
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-12-02 15:38:11 +0200

    add better autocard draw animation

--- src/routes/card-draw/+page.svelte
+++ src/routes/card-draw/+page.svelte
@@ -1,77 +1,37 @@
 <script>
     import { onMount } from "svelte";
+    import { gsap } from "gsap";
     import Button from "../../lib/components/Button.svelte";
     import CardBack from "../../lib/components/CardBack.svelte";
 
     let rarities = [
-        "common",
-        "rare",
-        "epic",
-        "legendary",
-        "mystical",
-        "rare",
-        "epic",
-        "common",
-        "legendary",
-        "mystical",
-        "legendary",
-        "mystical",
-        "rare",
+        "common", "epic", "rare", "mystical", "legendary", 
+        "epic", "common", "legendary", "rare", "mystical", 
+        "rare", "epic", "common", "mystical", "epic", 
+        "legendary", "rare", "epic", "common", "mystical", 
+        "legendary", "common", "epic", "rare", "legendary", 
+        "rare", "common", "mystical", "epic", "legendary",
+        "common", "epic", "rare", "mystical", "legendary", 
+        "epic", "common", "legendary", "rare", "mystical", 
+        "rare", "epic", "common", "mystical", "epic", 
+        "legendary", "rare", "epic", "common", "mystical", 
+        "legendary", "common", "epic", "rare", "legendary", 
+        "rare", "common", "mystical", "epic", "legendary"
     ];
 
-    let minSize = 15;
-
+    let cardWidth = $state();
+    let containerWidth = $state();
     let windowWidth = $state();
-    let middlePoint = $derived(windowWidth / 2);
-
-    // let w = $state();
 
     function scrollToEnd() {
+        let cardsOnRight = 1;
         const container = document.querySelector(".scroll-container");
-        container.scrollTo({ left: container.scrollWidth, behavior: "smooth" });
+
+        gsap.to(container, { x: -(container.scrollWidth - windowWidth/2 - cardWidth/2 - (cardsOnRight * (cardWidth + 8))), duration: 4, ease: "power2.inOut"});
     }
 
-    // function controlSize(node) {
-    //     $effect(() => {
-    //         let position = node.getBoundingClientRect().left;
-    //         let width = node.getBoundingClientRect().width;
-
-    //         const calculateCenter = () => {
-    //             let cardMiddle = position + width / 2;
-
-    //             let distance = Math.abs(middlePoint - cardMiddle);
-    //             let size = minSize + (1 / distance) * 1000;
-    //             console.log(size);
-    //             return size;
-    //         };
-
-    //         let size = calculateCenter();
-    //         node.style.height = `${size}rem`;
-    //     });
-    // }
-
-    function changeSize() {
-        let allCards = document.querySelectorAll(".size-change-element");
-
-        allCards.forEach((node) => {
-            let position = node.getBoundingClientRect().left;
-            let width = node.getBoundingClientRect().width;
-
-            const calculateCenter = () => {
-                let cardMiddle = position + width / 2;
-
-                let distance = Math.abs(middlePoint - cardMiddle);
-                let size = minSize + (1 / distance) * 1000;
-                console.log(size);
-                return size;
-            };
-
-            let size = calculateCenter();
-            node.style.height = `${size}rem`;
-        });
-    }
 </script>
-
+<!-- window size -->
 <svelte:window bind:innerWidth={windowWidth} />
 <main class="relative">
     <div class="flex flex-col w-full justify-center items-center">
@@ -82,12 +42,14 @@
                 class="w-8 h-8 -rotate-90" />
         </div>
         <div
-            class="my-5 min-h-96 flex flex-row items-center w-full overflow-x-auto overflow-y-hidden scroll-container"
-            onscroll={changeSize}>
+            bind:clientWidth={containerWidth}
+            class="min-h-96 flex flex-row items-center w-fit scroll-container gap-2 ">
             {#each rarities as rarity, i}
                 <div
-                    class="flex-shrink-0 z-10 transition-all ease-in-out delay-15 size-change-element"
-                    style:height="15rem">
+                    bind:clientWidth={cardWidth}
+                    class="flex-shrink-0 z-10 transition-all ease-in-out delay-300 cards"
+                    style:height="22rem"
+                    id="card_{i}">
                     <CardBack {rarity} />
                 </div>
             {/each}
