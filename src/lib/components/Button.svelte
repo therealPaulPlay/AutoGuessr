5ab570b28699333e71329d3d6609ae9655882dca
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-12-04 19:48:41 +0200

    add title to description

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -34,7 +34,7 @@
     style:width={buttonWidth}
     style:height={buttonHeight}>
     <button
-        class="rounded-lg z-10 ease-out duration-150 px-3 w-full flex items-center justify-center box-border {border ? "border border-white border-4" : ""}"
+        class="rounded-lg z-10 ease-out duration-150 px-3 w-full flex items-center justify-center box-border {border ? "border-white border-4" : ""}"
         bind:this={button}
         onpointerup={shiftButtonUp}
         onpointerdown={pressButtonDown}
@@ -48,7 +48,7 @@
     </button>
     <!-- Shadow/Depth -->
     <div
-        class="rounded-lg absolute w-full box-border {border ? "border border-white border-4" : ""}"
+        class="rounded-lg absolute w-full box-border {border ? "border-white border-4" : ""}"
         style:background-color={bgcolor}
         style="height: calc(100% - {shadowHeight}"
         style:margin-top={shadowHeight}>
--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,13 +1,12 @@
 <script>
-    let { 
-        images=[],
-        description=null,
-        descriptionFlag=false,
-        children
-     } = $props();
+    let {
+        images = [],
+        description = null,
+        descriptionFlag = false,
+        children,
+    } = $props();
 
     let currentIndex = $state(0);
-
 
     function next() {
         currentIndex = (currentIndex + 1) % images.length;
@@ -18,26 +17,52 @@
     }
 </script>
 
-<div class="flex w-full h-[60vh] relative overflow-clip rounded-2xl border-white border-8">
+<div
+    class="flex w-full h-[60vh] relative overflow-clip rounded-2xl border-white border-8"
+>
     {#if !descriptionFlag}
-        <img src={images[currentIndex]} alt="Carousel" class="absolute h-full w-full object-cover z-10"/>
+        <img
+            src={images[currentIndex]}
+            alt="Carousel"
+            class="absolute h-full w-full object-cover z-10"
+        />
     {/if}
-    <div class="relative w-full h-full overflow-hidden flex justify-between items-center gap-4 p-4 bg-white">
+    <div
+        class="relative w-full h-full overflow-hidden flex justify-between items-center gap-4 p-4 bg-white"
+    >
         {#if !descriptionFlag}
-        <div class="z-20 w-full h-full flex flex-row justify-between items-center">
-            <div class="realtive w-16 h-16 flex justify-center bg-white rounded-full pr-2">
-                <button onclick={prev} class="z-20">
-                    <img src="/assets/svg/arrow.svg" alt="Previous" class="w-8"/>
-                </button>
+            <div
+                class="z-20 w-full h-full flex flex-row justify-between items-center"
+            >
+                <div
+                    class="realtive w-16 h-16 flex justify-center bg-white rounded-full pr-2"
+                >
+                    <button onclick={prev} class="z-20">
+                        <img
+                            src="/assets/svg/arrow.svg"
+                            alt="Previous"
+                            class="w-8"
+                        />
+                    </button>
+                </div>
+                <div
+                    class="realtive w-16 h-16 flex justify-center bg-white rounded-full pl-2"
+                >
+                    <button onclick={next} class="z-20">
+                        <img
+                            src="/assets/svg/arrow.svg"
+                            alt="Next"
+                            class="w-8 scale-x-[-1]"
+                        />
+                    </button>
+                </div>
             </div>
-            <div class="realtive w-16 h-16 flex justify-center bg-white rounded-full pl-2">
-                <button onclick={next} class="z-20">
-                    <img src="/assets/svg/arrow.svg" alt="Next" class="w-8 scale-x-[-1]"/>
-                </button>
-            </div>
-        </div>
         {:else}
-            <div class="w-full h-full flex overflow-auto">
+            <div class="w-full h-full flex flex-col overflow-auto p-5">
+                <h1 class="text-orange font-bold text-2xl text-center mb-10">
+                    Here is what the seller has to say:
+                    <!-- Maybe we can make this dynamically change between different sentences? To make it feel fresh -->
+                </h1>
                 <p class="text-black text-lg">{description}</p>
             </div>
         {/if}
