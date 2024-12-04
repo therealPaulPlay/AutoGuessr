54b31e156f6a5ed1d0b91efdce42782133d656fe
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-12-04 19:38:58 +0200

    add button reveal animation

--- src/routes/card-draw/+page.svelte
+++ src/routes/card-draw/+page.svelte
@@ -1,9 +1,13 @@
 <script>
     import { onMount } from "svelte";
+    import { tick } from "svelte";
+    import { fade } from "svelte/transition";
     import { gsap } from "gsap";
     import Button from "../../lib/components/Button.svelte";
     import CardBack from "../../lib/components/CardBack.svelte";
     import Card from "../../lib/components/Card.svelte";
+    import Flip from "gsap/dist/Flip";
+    gsap.registerPlugin(Flip);
 
     let cardInfo = {
         name: "TestName 1",
@@ -15,29 +19,32 @@
         image: "/assets/img/example/cardcar.png",
     };
 
+    let cardWidth = $state();
+    let containerWidth = $state();
+    let windowWidth = $state();
     let raritiesSize = 40;
     let rarities = [];
-    let showRevealButton = $state(true);
+    let showRevealButton = $state(false);
     let showCardBack = $state(false);
-    let cardsOnRight = 3;  // For some reason 0 breaks it. Not sure why
+    let cardsOnRight = 3; // For some reason 0 breaks it. Not sure why
     let cardPositionIndex = raritiesSize - 1 - cardsOnRight;
 
     for (let i = 0; i < raritiesSize; i++) {
         let number = Math.floor(Math.random() * 100);
         switch (true) {
-            case (i === cardPositionIndex):
+            case i === cardPositionIndex:
                 rarities.push(cardInfo.rarity);
                 break;
-            case (number <= 50):
+            case number <= 50:
                 rarities.push("common");
                 break;
-            case (number <= 70):
+            case number <= 70:
                 rarities.push("rare");
                 break;
-            case (number <= 90):
+            case number <= 90:
                 rarities.push("epic");
                 break;
-            case (number <= 98):
+            case number <= 98:
                 rarities.push("legendary");
                 break;
             default:
@@ -45,10 +52,6 @@
                 break;
         }
     }
-
-    let cardWidth = $state();
-    let containerWidth = $state();
-    let windowWidth = $state();
 
     function scrollToEnd() {
         const container = document.querySelector(".scroll-container");
@@ -62,10 +65,13 @@
                 container.scrollWidth -
                 windowWidth / 2 -
                 cardWidth / 2 -
-                (cardsOnRight * (cardWidth + 8))
+                cardsOnRight * (cardWidth + 8)
             ),
             duration: 4,
             ease: "power2.inOut",
+        }).then(() => {
+            showRevealButton = true;
+            buttonAnimation();
         });
     }
 
@@ -80,8 +86,17 @@
             duration: 0.5,
             ease: "power2.inOut",
         });
+    }
 
-        console.log("yes");
+    async function buttonAnimation() {
+        const revealBtnState = Flip.getState(".btn");
+
+        await tick();
+
+        Flip.from(revealBtnState, {
+            duration: 0.3,
+            ease: "power1.inOut",
+        });
     }
 
     onMount(() => {
@@ -100,19 +115,23 @@
         resizeTimeout = setTimeout(() => {
             initialCenterCards();
         }, 100);
-    }} />
+    }}
+/>
 <content class="relative h-full block">
     <div
-        class="flex flex-col w-full justify-center items-center custom-card-container-height">
+        class="flex flex-col w-full justify-center items-center custom-card-container-height"
+    >
         <div>
             <img
                 src="/assets/svg/arrow.svg"
                 alt="Arrow"
-                class="w-8 h-8 -rotate-90" />
+                class="w-8 h-8 -rotate-90"
+            />
         </div>
         <div
             bind:clientWidth={containerWidth}
-            class="min-h-96 flex flex-row items-center w-fit scroll-container gap-2 my-5">
+            class="min-h-96 flex flex-row items-center w-fit scroll-container gap-2 my-5"
+        >
             {#each rarities as rarity, i}
                 {#if i === cardPositionIndex}
                     <div
@@ -121,13 +140,16 @@
                         class:flip-it={showCardBack}
                         style:height="24rem"
                         style:width="{cardWidth}px"
-                        id="card_{i}">
+                        id="card_{i}"
+                    >
                         <div
-                            class="absolute w-full h-full [backface-visibility:hidden]">
+                            class="absolute w-full h-full [backface-visibility:hidden]"
+                        >
                             <CardBack {rarity} />
                         </div>
                         <div
-                            class="absolute w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)]">
+                            class="absolute w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)]"
+                        >
                             <Card {...cardInfo} />
                         </div>
                     </div>
@@ -136,7 +158,8 @@
                         bind:clientWidth={cardWidth}
                         class="flex-shrink-0 z-10 transition-all ease-in-out delay-300"
                         style:height="24rem"
-                        id="card_{i}">
+                        id="card_{i}"
+                    >
                         <CardBack {rarity} />
                     </div>
                 {/if}
@@ -146,26 +169,35 @@
             <img
                 src="/assets/svg/arrow.svg"
                 alt="Arrow"
-                class="w-8 h-8 rotate-90 scale-y-[-1]" />
+                class="w-8 h-8 rotate-90 scale-y-[-1]"
+            />
         </div>
-        <div class="mt-5 flex flex-row gap-5">
-            <Button
-                buttonHeight="4rem"
-                buttonWidth="12rem"
-                shadowHeight="0.5rem"
-                execFunction={scrollToEnd}>
-                <span class="text-white font-bold text-4xl">Roll!</span>
-            </Button>
-            {#if showRevealButton}
+        <div class="mt-5 flex flex-row gap-5 btn">
+            <div class="min-w-[12rem]">
                 <Button
                     buttonHeight="4rem"
                     buttonWidth="12rem"
                     shadowHeight="0.5rem"
-                    color="var(--green-button)"
-                    bgcolor="var(--green-button-dark)"
-                    execFunction={revealCard}>
-                    <span class="text-white font-bold text-4xl">Reveal!</span>
+                    execFunction={scrollToEnd}
+                >
+                    <span class="text-white font-bold text-4xl">Roll!</span>
                 </Button>
+            </div>
+            {#if showRevealButton}
+                <div transition:fade>
+                    <Button
+                        buttonHeight="4rem"
+                        buttonWidth="12rem"
+                        shadowHeight="0.5rem"
+                        color="var(--green-button)"
+                        bgcolor="var(--green-button-dark)"
+                        execFunction={revealCard}
+                    >
+                        <span class="text-white font-bold text-4xl"
+                            >Reveal!</span
+                        >
+                    </Button>
+                </div>
             {/if}
         </div>
     </div>
@@ -173,23 +205,28 @@
         <img
             src="/assets/svg/question mark.svg"
             alt="Background question mark"
-            class="absolute h-52 bottom-0 rotate-12" />
+            class="absolute h-52 bottom-0 rotate-12"
+        />
         <img
             src="/assets/svg/question mark.svg"
             alt="Background question mark"
-            class="absolute h-36 bottom-0 left-36 rotate-45" />
+            class="absolute h-36 bottom-0 left-36 rotate-45"
+        />
         <img
             src="/assets/svg/question mark.svg"
             alt="Background question mark"
-            class="absolute h-36 -top-32 left-72 -rotate-12 scale-y-[-1] scale-x-[-1]" />
+            class="absolute h-36 -top-32 left-72 -rotate-12 scale-y-[-1] scale-x-[-1]"
+        />
         <img
             src="/assets/svg/question mark.svg"
             alt="Background question mark"
-            class="absolute h-24 -top-32 left-56 scale-y-[-1] scale-x-[-1]" />
+            class="absolute h-24 -top-32 left-56 scale-y-[-1] scale-x-[-1]"
+        />
         <img
             src="/assets/svg/question mark.svg"
             alt="Background question mark"
-            class="absolute h-28 bottom-0 right-8 -rotate-12" />
+            class="absolute h-28 bottom-0 right-8 -rotate-12"
+        />
     </div>
 </content>
 
