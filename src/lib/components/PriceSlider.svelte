00da085c7a529615b08ca8fda6dbbf73f0c3bfcb
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-12-10 16:16:10 +0200

    pass guess value from slider to parent

--- src/lib/components/PriceSlider.svelte
+++ src/lib/components/PriceSlider.svelte
@@ -1,13 +1,21 @@
 <script>
-    let { sliderMin = 1, sliderMax = 500_000, children } = $props();
+    let {
+        sliderMin = 1,
+        sliderMax = 500_000,
+        guessValue = $bindable(null),
+        children,
+    } = $props();
 
     const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
 
     let guess = $state(sliderMin);
     let restrictedGuess = $derived(clamp(guess, sliderMin, sliderMax));
+    $effect(() => {
+        guessValue = restrictedGuess;
+    });
 
     function onGuessChange() {
-        console.log("change");
+        
     }
 
     function formatedGuess() {
@@ -16,15 +24,15 @@
 
     function preventNonNumericalInput(e) {
         const allowedKeys = [
-            'Backspace',
-            'Delete',
-            'ArrowLeft',
-            'ArrowRight',
-            'Tab',
-            'Home',
-            'End'
+            "Backspace",
+            "Delete",
+            "ArrowLeft",
+            "ArrowRight",
+            "Tab",
+            "Home",
+            "End",
         ];
-        
+
         if (allowedKeys.includes(e.key)) return;
         // For other keys, only allow numbers
         if (!e.key.match(/^[0-9]$/)) e.preventDefault();
@@ -38,7 +46,8 @@
 </script>
 
 <div
-    class="flex items-center h-[4.2rem] rounded-lg bg-tanLight p-2 drop-shadow-[0_0.3rem_0_var(--white-shadow)] gap-2">
+    class="flex items-center h-[4.2rem] rounded-lg bg-tanLight p-2 drop-shadow-[0_0.3rem_0_var(--white-shadow)] gap-2"
+>
     <div class="grow-[2]">
         <input
             type="range"
@@ -47,12 +56,19 @@
             max={sliderMax}
             bind:value={guess}
             onchange={onGuessChange}
-            class="slider w-full" />
+            class="slider w-full"
+        />
     </div>
     <div
-        class="flex items-center justify-center relative bg-white text-xl p-2 rounded-md h-full text-orange font-bold min-w-40 transition {inputFocused ? "!bg-orange" : ""}">
-        <span class="w-full h-full bg-transparent text-center custom-vertical-align {inputFocused ? "text-white" : ""} transition"
-            >{formatedGuess()}</span>
+        class="flex items-center justify-center relative bg-white text-xl p-2 rounded-md h-full text-orange font-bold min-w-40 transition {inputFocused
+            ? '!bg-orange'
+            : ''}"
+    >
+        <span
+            class="w-full h-full bg-transparent text-center custom-vertical-align {inputFocused
+                ? 'text-white'
+                : ''} transition">{formatedGuess()}</span
+        >
         <input
             type="number"
             maxlength="8"
@@ -60,10 +76,15 @@
             onchange={formatedGuess}
             onkeydown={preventNonNumericalInput}
             onclick={selectAllText}
-            onfocus={() => {inputFocused = true}}
-            onfocusout={() => {inputFocused = false}}
+            onfocus={() => {
+                inputFocused = true;
+            }}
+            onfocusout={() => {
+                inputFocused = false;
+            }}
             class="absolute w-full h-full"
-            style="opacity: 0;" />
+            style="opacity: 0;"
+        />
     </div>
 </div>
 
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -4,7 +4,9 @@
     import Stat from "$lib/components/Stat.svelte";
     import PriceSlider from "$lib/components/PriceSlider.svelte";
     import Button from "$lib/components/Button.svelte";
+    import Popup from "$lib/components/Popup.svelte";
     import { Check } from "lucide-svelte";
+    import { fly, slide } from "svelte/transition";
 
     let images = [
         "/assets/img/example/img1.jpeg",
@@ -47,7 +49,8 @@
 
     let description =
         "What does the buzzword 'technologies' really mean? Think virally-distributed. Quick: do you have a plan to become cross-media? We think that most co-branded splash pages use far too much Perl, and not enough OWL. Without niches, you will lack experiences. The capability to implement wirelessly leads to the ability to iterate virtually. Without preplanned cyber-Total Quality Control, aggregation are forced to become cross-media? We think that most C2C2C web-based applications use far too much Rails, and not enough PNG. Is it more important for something to be best-of-breed? The portals factor can be delivered as-a-service to wherever it’s intended to go – mobile. Our infinitely reconfigurable feature set is unmatched in the industry, but our back-end performance and non-complex use is invariably considered a remarkable achievement. It sounds wonderful, but it's 100 percent accurate! The experiences factor is 1000/60/60/24/7/365. Do you have a infinitely reconfigurable feature set is unparalleled, but our vertical, customized efficient, user-centric TQM and non-complex use is usually considered an amazing achievement. Do you have a infinitely reconfigurable scheme for coping with emerging methodologies? Is it more important for something to be customer-directed? What does the term 'dot-com' really mean? Helping marketers serve unmatched cross-phase personalized experiences at every step of the pudding is in the industry, but our C2C2C paradigms and easy configuration is usually considered an amazing achievement";
-
+    let resultPopup = $state(false);
+    let nextFlag = $state(false);
     let imageTab = $state(true);
     let descriptionFlag = $state(false);
     let lives = $state(2); // 0 to 3
@@ -63,6 +66,16 @@
         imageTab = false;
         descriptionFlag = true;
     }
+
+    function showResult() {
+        resultPopup = true;
+
+        setTimeout(() => {
+            nextFlag = true;
+        }, 1000);
+    }
+
+    let guessResult = $state(1);
 </script>
 
 <content class="items-center relative overflow-visible">
@@ -73,29 +86,35 @@
             <div class="flex w-1/2 gap-2 ml-4">
                 <Tab
                     color={imageTab ? "var(--white)" : "var(--default-shadow)"}
-                    onHover={false}
+                    tabStatic={true}
                     shadow={false}
-                    execFunction={displayImages}>
+                    execFunction={displayImages}
+                >
                     <span class="text-xl font-medium text-black">Images</span>
                 </Tab>
                 <Tab
                     color={imageTab ? "var(--default-shadow)" : "var(--white)"}
-                    onHover={false}
+                    tabStatic={true}
                     shadow={false}
-                    execFunction={displayDescription}>
+                    execFunction={displayDescription}
+                >
                     <span class="text-xl font-medium text-black"
-                        >Description</span>
+                        >Description</span
+                    >
                 </Tab>
             </div>
             <div class="flex flex-col w-full md:flex-row gap-5">
                 <div
-                    class="md:w-2/3 drop-shadow-[0_0.5rem_0_var(--default-shadow)]">
+                    class="md:w-2/3 drop-shadow-[0_0.5rem_0_var(--default-shadow)]"
+                >
                     <Carousel {images} {description} {descriptionFlag} />
                 </div>
                 <div
-                    class="md:w-1/3 md:mb-0 mb-96 overflow-auto drop-shadow-[0_0.5rem_0_var(--default-shadow)] rounded-2xl bg-white">
+                    class="md:w-1/3 md:mb-0 mb-96 overflow-auto drop-shadow-[0_0.5rem_0_var(--default-shadow)] rounded-2xl bg-white"
+                >
                     <div
-                        class="w-full flex flex-col justify-between h-fit gap-2 p-2">
+                        class="w-full flex flex-col justify-between h-fit gap-2 p-2"
+                    >
                         {#each stats as stat}
                             <Stat icon={stat.icon} text={stat.text} />
                         {/each}
@@ -106,25 +125,29 @@
     </div>
     <!-- Bottom UI -->
     <div
-        class="fixed bottom-0 margin-x-auto flex flex-row-reverse flex-wrap w-full md:gap-10 md:justify-center">
+        class="fixed bottom-0 margin-x-auto flex flex-row-reverse flex-wrap w-full md:gap-10 md:justify-center"
+    >
         <!-- Lives -->
         <div class="lives">
             <img
                 src="/assets/svg/traffic {lives}.svg"
                 alt="lives"
-                class="w-52 h-28 flex content-end" />
+                class="w-52 h-28 flex content-end"
+            />
         </div>
         <div
             class="p-2.5 rounded-t-2xl w-fit flex max-w-3xl"
-            style:background-color="var(--default-shadow)">
+            style:background-color="var(--default-shadow)"
+        >
             <div class="flex grow gap-2.5 text-white">
-                <PriceSlider min="0" max="10" />
+                <PriceSlider min="0" max="10" bind:guessValue={guessResult} />
                 <Button
                     color="var(--default-button)"
                     bgcolor="var(--default-button-dark)"
                     buttonHeight="4.5rem"
                     buttonWidth="5.5rem"
-                    execFunction={() => console.log("Guessed!")}>
+                    execFunction={showResult}
+                >
                     <span class="check-align-vertical">
                         <Check strokeWidth={5} size={35} />
                     </span>
@@ -133,6 +156,65 @@
         </div>
     </div>
 </content>
+
+<!-- Result Popups -->
+{#if resultPopup}
+    <Popup
+        title="Round results"
+        closeFunction={() => {
+            resultPopup = false;
+        }}
+    >
+        <div class="h-full flex flex-col justify-evenly items-center">
+            <div class="flex flex-col items-center">
+                {#if nextFlag}
+                    <p
+                        transition:fly={{ y: -50 }}
+                        class="font-bold text-green text-7xl mb-5"
+                    >
+                        {"Not bad!".toUpperCase()}
+                    </p>
+                    <p
+                        transition:slide={{ delay: 500 }}
+                        class="text-black text-base"
+                    >
+                        Your guess of <span class="text-orange font-semibold"
+                            >$5</span
+                        >
+                        was only
+                        <span class="text-orange font-semibold">10% off.</span>
+                    </p>
+                    <p
+                        transition:slide={{ delay: 500 }}
+                        class="text-black text-base"
+                    >
+                        You get <span class="text-green font-semibold"
+                            >500 points.</span
+                        >
+                    </p>
+                {/if}
+            </div>
+            <div>
+                <h1 class="font-bold text-black text-4xl">
+                    scrolling bar thingy here...
+                </h1>
+            </div>
+            {#if nextFlag}
+                <div transition:fly={{ x: -50, delay: 1000 }}>
+                    <Button
+                        buttonWidth="8rem"
+                        execFunction={() => {
+                            nextFlag = true;
+                            resultPopup = false;
+                        }}
+                    >
+                        <span class="text-white text-xl font-medium">Next</span>
+                    </Button>
+                </div>
+            {/if}
+        </div>
+    </Popup>
+{/if}
 
 <style>
     @media (max-width: 768px) {
