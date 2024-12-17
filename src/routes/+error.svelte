18f25df1f861faa45fb16d94081ee58ce64af83a
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-12-17 15:58:49 +0200

    better error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,29 @@
 <script>
     import { page } from "$app/stores";
+    import { ArrowRightCircle } from "lucide-svelte";
+    import { goto } from "$app/navigation";
+    import Button from "$lib/components/Button.svelte";
 </script>
 
-<div class="flex justify-center items-center">
-    <h1 class="font-bold text-xl">{$page.status}: {$page.error.message}</h1>
+<div class="flex flex-col items-center h-[80dvh] justify-around">
+    <div class="">
+        <h1 class="text-[15rem] text-orange leading-none text-center">{$page.status}</h1>
+        <h1 class="font-bold text-6xl text-orange text-center">{$page.error.message}</h1>
+    </div>
+    <Button
+        buttonWidth="12rem"
+        buttonHeight="4rem"
+        color="var(--green-button)"
+        bgcolor="var(--green-button-dark)"
+        execFunction={() => goto("/")}
+    >
+        <span
+            class="text-white text-xl font-medium flex items-center justify-center gap-2"
+        >
+            <div class="scale-x-[-1]">
+                <ArrowRightCircle strokeWidth={3} />
+            </div>
+            Main page</span
+        >
+    </Button>
 </div>
