ecaf4cf11c29ef4e47a52cceb8335c8dbc4215dc
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2024-12-23 21:55:26 +0200

    add blinking effect for lives display when at two lives

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -71,8 +71,10 @@
     let resultPopup = $state(false);
     let rewardFlag = $state(false);
     let penaltyFlag = $state(false);
+    let blinkingFlag = $state(false);
     let imageTab = $state(true);
     let descriptionFlag = $state(false);
+    let blinkingLives = $state();
     score.set(0);
 
     function displayImages() {
@@ -186,6 +188,19 @@
         }
     }
 
+    $effect(() => {
+        // Makes currentLives switch between 2 and 3 on 2 lives
+        // Using a separate variable to $lives because using it may cause issues.
+        if ($lives === 2) {
+            blinkingFlag = true;
+            setInterval(() => {
+                blinkingLives = blinkingLives === 2 ? 3 : 2; // Toggle between 2 and 3
+            }, 1000);
+        } else {
+            blinkingFlag = false;
+        }
+    });
+
     let guessResult = $state(1);
 
     onMount(() => {
@@ -206,29 +221,36 @@
                 <Tab
                     color={imageTab ? "var(--white)" : "var(--default-shadow)"}
                     shadow={false}
-                    execFunction={displayImages}>
+                    execFunction={displayImages}
+                >
                     <span class="text-xl font-medium text-black">Images</span>
                 </Tab>
                 <Tab
                     color={imageTab ? "var(--default-shadow)" : "var(--white)"}
                     shadow={false}
-                    execFunction={displayDescription}>
+                    execFunction={displayDescription}
+                >
                     <span class="text-xl font-medium text-black"
-                        >Description</span>
+                        >Description</span
+                    >
                 </Tab>
             </div>
             <div class="flex flex-col w-full md:flex-row gap-5">
                 <div
-                    class="md:w-2/3 drop-shadow-[0_0.5rem_0_var(--default-shadow)]">
+                    class="md:w-2/3 drop-shadow-[0_0.5rem_0_var(--default-shadow)]"
+                >
                     <Carousel
                         images={question.images}
                         description={question.description}
-                        {descriptionFlag} />
+                        {descriptionFlag}
+                    />
                 </div>
                 <div
-                    class="md:w-1/3 md:mb-0 mb-96 overflow-auto drop-shadow-[0_0.5rem_0_var(--default-shadow)] rounded-2xl bg-white">
+                    class="md:w-1/3 md:mb-0 mb-96 overflow-auto drop-shadow-[0_0.5rem_0_var(--default-shadow)] rounded-2xl bg-white"
+                >
                     <div
-                        class="w-full flex flex-col justify-between h-fit gap-2 p-2">
+                        class="w-full flex flex-col justify-between h-fit gap-2 p-2"
+                    >
                         {#each question.stats as stat}
                             <Stat icon={stat.icon} text={stat.text} />
                         {/each}
@@ -239,17 +261,21 @@
     </div>
     <!-- Bottom UI -->
     <div
-        class="fixed bottom-0 margin-x-auto flex flex-row-reverse flex-wrap w-full md:gap-10 md:justify-center">
+        class="fixed bottom-0 margin-x-auto flex flex-row-reverse flex-wrap w-full md:gap-10 md:justify-center"
+    >
         <!-- Lives -->
         <div class="lives">
+            <!-- This is one of the worst things I've wrote. Is there another way? -->
             <img
-                src="/assets/svg/traffic {$lives}.svg"
+                src="/assets/svg/traffic {blinkingFlag ? blinkingLives : $lives < 2 ? $lives+1 : $lives}.svg"
                 alt="lives"
-                class="w-52 h-28 flex content-end" />
+                class="w-52 h-28 flex content-end"
+            />
         </div>
         <div
             class="p-2.5 rounded-t-2xl w-fit flex max-w-3xl"
-            style:background-color="var(--default-shadow)">
+            style:background-color="var(--default-shadow)"
+        >
             <div class="flex grow gap-2.5 text-white">
                 <PriceSlider min="0" max="10" bind:guessValue={guessResult} />
                 <Button
@@ -257,7 +283,8 @@
                     bgcolor="var(--default-button-dark)"
                     buttonHeight="4.5rem"
                     buttonWidth="5.5rem"
-                    execFunction={showResult}>
+                    execFunction={showResult}
+                >
                     <span class="check-align-vertical">
                         <Check strokeWidth={5} size={30} />
                     </span>
@@ -274,26 +301,37 @@
             <div class="flex flex-col items-center">
                 <p
                     in:fly={{ y: -50, delay: 500 }}
-                    class="font-bold text-green text-7xl mb-5 text-center">
+                    class="font-bold text-green text-7xl mb-5 text-center"
+                >
                     {"Not bad!".toUpperCase()}
                 </p>
-                <p class="text-black text-base text-center" in:fly={{ y: -50, delay: 500 }}>
+                <p
+                    class="text-black text-base text-center"
+                    in:fly={{ y: -50, delay: 500 }}
+                >
                     Your guess of <span class="text-orange font-semibold"
-                        >${guessResult.toLocaleString()}</span>
+                        >${guessResult.toLocaleString()}</span
+                    >
                     was only
                     <span class="text-orange font-semibold"
-                        >{percentageDifference().toFixed(2)}% off.</span>
+                        >{percentageDifference().toFixed(2)}% off.</span
+                    >
                 </p>
-                <p in:slide={{ delay: 250 }} class="text-black text-base text-center">
+                <p
+                    in:slide={{ delay: 250 }}
+                    class="text-black text-base text-center"
+                >
                     You get <span class="text-green font-semibold"
                         >{pointCalculation()} points{rewardFlag
                             ? " and an extra life!"
-                            : "."}</span>
+                            : "."}</span
+                    >
                     {#if penaltyFlag}
                         <span class="font-semibold text-red-600 decoration-2"
                             >You lose {$difficulty === 3
                                 ? "2 lives."
-                                : "a life."}</span>
+                                : "a life."}</span
+                        >
                     {/if}
                 </p>
             </div>
@@ -301,16 +339,19 @@
                 <GuessDisplay
                     answer={question.answer}
                     guess={guessResult}
-                    percentageDifference={percentageDifference().toFixed(2)} />
+                    percentageDifference={percentageDifference().toFixed(2)}
+                />
             </div>
             <div in:fly={{ x: -50, delay: 2500 }}>
                 <Button
                     buttonWidth="12rem"
                     buttonHeight="4rem"
-                    execFunction={goToNextQuestion}>
+                    execFunction={goToNextQuestion}
+                >
                     <span
                         class="text-white text-xl font-medium flex items-center justify-center gap-2"
-                        >Next <ArrowRightCircle strokeWidth={3} /></span>
+                        >Next <ArrowRightCircle strokeWidth={3} /></span
+                    >
                 </Button>
             </div>
         </div>
