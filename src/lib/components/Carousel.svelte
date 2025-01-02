e5aa35bd609f7d047845bcaa06bc6cb0fc0769e7
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2025-01-02 20:17:07 +0200

    add image zoom functionality

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,4 +1,7 @@
 <script>
+    import { onMount } from "svelte";
+    import { fade } from 'svelte/transition';
+
     let {
         images = [],
         description = null,
@@ -6,6 +9,9 @@
         children,
     } = $props();
 
+    let imgElement = $state();
+    let zoomResult = $state();
+    let showZoom = $state(false);
     let currentIndex = $state(0);
 
     function next() {
@@ -16,7 +22,57 @@
         currentIndex = (currentIndex - 1 + images.length) % images.length;
     }
 
+    function imageZoom() {
+        var img = imgElement;
+        var result = zoomResult;
+
+        /* Set the zoomResult's background to show the image */
+        result.style.backgroundImage = `url(${img.src})`;
+        result.style.backgroundSize =
+            img.width * 2 + "px " + img.height * 2 + "px";
+
+        img.addEventListener("mousemove", updateZoom);
+        img.addEventListener("mouseleave", () => {
+            showZoom = false;
+        });
+
+        function updateZoom(e) {
+            /* Get the cursor's position relative to the image */
+            var pos = getCursorPos(e);
+
+            /* Calculate the percentage position of the cursor on the image */
+            var xPercent = (pos.x / img.width) * 100;
+            var yPercent = (pos.y / img.height) * 100;
+
+            if(xPercent > 50) {
+                result.style.left = "0";
+            } else {
+                result.style.left = "";
+            }
+
+            /* Set the background position of the zoomResult */
+            result.style.backgroundPosition = `${xPercent}% ${yPercent}%`;
+        }
+
+        function getCursorPos(e) {
+            var a,
+                x = 0,
+                y = 0;
+            e = e || window.event;
+            a = img.getBoundingClientRect();
+            x = e.pageX - a.left - window.pageXOffset;
+            y = e.pageY - a.top - window.pageYOffset;
+            return { x: x, y: y };
+        }
+    }
+
     let imageFit = $state(false);
+
+    $effect(() => {
+        if (imgElement && showZoom) {
+            imageZoom();
+        }
+    });
 </script>
 
 <div
@@ -28,13 +84,29 @@
         <img
             src={images[currentIndex]}
             alt="Loading images..."
+            bind:this={imgElement}
             onclick={() => {
                 imageFit = !imageFit;
             }}
+            onmouseover={() => {
+                showZoom = true;
+            }}
+            onfocus={() => {
+                showZoom = true;
+            }}
             tabindex="0"
             role="button"
-            class="absolute h-full w-full {imageFit ? "object-contain" : "object-cover"} z-10 bg-white cursor-pointer rounded-lg"
+            class="absolute h-full w-full {imageFit
+                ? 'object-contain'
+                : 'object-cover'} z-10 bg-white cursor-crosshair rounded-lg"
         />
+        {#if showZoom}
+            <div
+                transition:fade={{ duration: 150 }}
+                bind:this={zoomResult}
+                class="absolute m-5 w-32 h-32 z-10 rounded-lg right-0"
+            ></div>
+        {/if}
     {/if}
     <div
         class="relative w-full h-full overflow-hidden flex justify-between items-center bg-tanLight rounded-lg"
@@ -46,7 +118,10 @@
                 <div
                     class="realtive w-16 h-16 flex justify-center bg-white rounded-full pr-2 pointer-events-auto"
                 >
-                    <button onclick={prev} class="z-20 transition active:scale-90">
+                    <button
+                        onclick={prev}
+                        class="z-20 transition active:scale-90"
+                    >
                         <img
                             src="/assets/svg/arrow.svg"
                             alt="Previous"
@@ -57,7 +132,10 @@
                 <div
                     class="realtive w-16 h-16 flex justify-center bg-white rounded-full pl-2 pointer-events-auto"
                 >
-                    <button onclick={next} class="z-20 transition active:scale-90">
+                    <button
+                        onclick={next}
+                        class="z-20 transition active:scale-90"
+                    >
                         <img
                             src="/assets/svg/arrow.svg"
                             alt="Next"
@@ -77,3 +155,13 @@
         {/if}
     </div>
 </div>
+
+<style>
+    .img-zoom-lens {
+        position: absolute;
+        border: 1px solid #d4d4d4;
+        /*set the size of the lens:*/
+        width: 100px;
+        height: 100px;
+    }
+</style>
