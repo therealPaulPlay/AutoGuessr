3f8cc85373ac2579c953e9c25f6da85db0842af3
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2025-01-03 13:33:26 +0200

    add basic setting options

---
+++ src/lib/components/Toggle.svelte
@@ -0,0 +1,72 @@
+<script>
+</script>
+
+<label class="switch">
+    <input type="checkbox" />
+    <span class="switch-slider round"></span>
+</label>
+
+<style>
+    /* The switch - the box around the slider */
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+    }
+
+    /* Hide default HTML checkbox */
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+    /* The slider */
+    .switch-slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: 34px;
+        background-color: #ccc;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+    }
+
+    .switch-slider:before {
+        position: absolute;
+        content: "";
+        height: 26px;
+        width: 26px;
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+    }
+
+    input:checked + .switch-slider {
+        background-color: var(--default-button);
+    }
+
+    input:focus + .switch-slider {
+        box-shadow: 0 0 1px #2196f3;
+    }
+
+    input:checked + .switch-slider:before {
+        -webkit-transform: translateX(26px);
+        -ms-transform: translateX(26px);
+        transform: translateX(26px);
+    }
+
+    /* Rounded sliders */
+    .switch-slider.round {
+        border-radius: 9999px;
+    }
+
+    .switch-slider.round:before {
+        border-radius: 50%;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,10 +14,12 @@
     import { errorPopup, errorText } from "$lib/stores/uiStore";
     import { displayError } from "$lib/utils/displayError";
     import { checkAuthenticationStatus } from "$lib/utils/checkAuthStatus";
-    import { User, TriangleAlert } from "lucide-svelte";
+    import { User, TriangleAlert, Scan } from "lucide-svelte";
     import { onMount } from "svelte";
     import SignupPopup from "$lib/components/signupPopup.svelte";
     import AccountPopup from "$lib/components/AccountPopup.svelte";
+    import PriceSlider from "$lib/components/PriceSlider.svelte";
+    import Toggle from "$lib/components/Toggle.svelte";
 
     let { children } = $props();
 
@@ -116,11 +118,60 @@
 {#if $settingsPopup}
     <Popup
         title="Settings"
+        small="true"
         closeFunction={() => {
             settingsPopup.set(false);
         }}
     >
-        <!-- Settings contents... -->
+        <div class="flex h-full items-end">
+            <div class="h-4/6 w-full p-4 flex flex-col gap-5">
+                <div class="flex items-center gap-10 justify-between">
+                    <label for="volume-slider" class="text-base font-semibold"
+                        >Volume</label
+                    >
+                    <div class="w-1/2">
+                        <input
+                            type="range"
+                            id="volume-slider"
+                            min="0"
+                            max="100"
+                            value="50"
+                            step="10"
+                            class="slider"
+                        />
+                    </div>
+                </div>
+                <div class="flex items-center gap-10 justify-between">
+                    <label for="something" class="text-base font-semibold"
+                        >Something</label
+                    >
+                    <div class="w-1/2 flex justify-center">
+                        <Toggle />
+                    </div>
+                </div>
+                <div class="flex items-center gap-10 justify-between">
+                    <label for="fullscreen" class="text-base font-semibold"
+                        >Fullscreen</label
+                    >
+                    <div class="w-1/2 flex justify-center">
+                        <Button
+                            color="var(--default-button)"
+                            bgcolor="var(--default-button-dark)"
+                            execFunction={() => {
+                                // Toggle on/off fullscreen
+                                if (document.fullscreenElement) {
+                                    document.exitFullscreen();
+                                } else {
+                                    document.documentElement.requestFullscreen();
+                                }
+                            }}
+                        >
+                            <Scan strokeWidth={3} color="var(--white)" />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </Popup>
 {/if}
 
