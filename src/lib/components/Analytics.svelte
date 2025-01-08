65cbbdd3b410c72cbdf361a9d8d44c9827dc3aad
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-01-09 00:28:56 +0100

    privacy policy and first part of google analytics integration

---
+++ src/lib/components/Analytics.svelte
@@ -0,0 +1,92 @@
+<script>
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+  import { AlertCircle } from "lucide-svelte";
+  import Button from "./Button.svelte";
+
+  let showBanner = $state(false);
+  let cookieConsent = false;
+
+  // Instead of a banner, if one CMP already exists (e.g. for Ads), use TFC instead to listen to the consent from there
+  // window["gtag_enable_tcf_support"] = true;
+
+  onMount(() => {
+    // Check if the user has already accepted cookies
+    const consent = localStorage.getItem("cookieConsent");
+
+    if (consent) {
+      cookieConsent = true;
+      loadGoogleAnalytics(true);
+    } else {
+      showBanner = true;
+      loadGoogleAnalytics(false);
+    }
+  });
+
+  function loadGoogleAnalytics(hasConsent) {
+    try {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      const id = "G-BXJL38P1X3";
+
+      gtag("js", new Date());
+      gtag("config", id, {
+        page_title: document.title,
+        page_path: $page.url.pathname,
+        cookie_domain: location.hostname,
+        cookie_flags: "SameSite=None; Secure",
+        // Deny data collection by default
+        ad_storage: "denied",
+        ad_personalization: "denied",
+        ad_user_data: "denied",
+        analytics_storage: "denied",
+      });
+
+      // Grant permissions if the user has accepted
+      if (hasConsent) {
+        gtag("config", id, {
+          ad_storage: "granted",
+          ad_personalization: "granted",
+          ad_user_data: "granted",
+          analytics_storage: "granted",
+        });
+      }
+    } catch (error) {
+      console.error("Error loading Google Analytics:", error);
+    }
+  }
+
+  function handleAccept() {
+    cookieConsent = true;
+    localStorage.setItem("cookieConsent", "true");
+    loadGoogleAnalytics(true);
+    showBanner = false;
+  }
+
+  function handleDeny() {
+    cookieConsent = false;
+    localStorage.setItem("cookieConsent", "false");
+    showBanner = false;
+  }
+</script>
+
+<svelte:head>
+  <script src="https://www.googletagmanager.com/gtag/js?id=G-DXD64B7ZDX" async></script>
+</svelte:head>
+
+{#if showBanner}
+  <div role="alert" class="alert fixed w-fit m-4 bottom-0 bg-base-100 right-0 z-50 shadow-lg">
+    <AlertCircle className="stroke-info h-6 w-6 shrink-0" />
+    <span
+      >This website uses cookies according to its <a href="https://openguessr.com/legal" class="text-secondary"
+        >privacy policy</a
+      >.</span
+    >
+    <div class="flex flex-wrap gap-2">
+      <Button onclick={handleDeny}>Only essentia</Button>
+      <Button onclick={handleAccept}>Accept</Button>
+    </div>
+  </div>
+{/if}
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,123 @@
+<svelte:head>
+  <title>Privacy Policy</title>
+</svelte:head>
+
+<div class="w-full flex justify-center p-4">
+  <div class="mx-auto max-w-screen-lg rounded-md bg-white p-8 rounded-xl mb-5">
+    <h1 class="font-bold text-2xl my-2">Introduction</h1>
+    <p>
+      This page exists to inform the reader about how user data is collected, which terms apply when using the game, and
+      how to reach out to the developer. By using this website, you accept the following agreements and policies. Please
+      note that some information is available on different pages linked below. If you have any questions regarding these
+      policies, feel free to reach out using the contact form, which you can find linked at the bottom of this page.
+    </p>
+    <br />
+    <p>
+      Imprint: For imprint and contact information, please refer to <a
+        href="https://paulplay.studio/imprint"
+        target="_blank"
+        class="underline">this page</a
+      >.
+    </p>
+    <p>
+      Terms of Use: For the Terms of Use, please refer to <a
+        href="https://paulplay.studio/terms-of-use"
+        target="_blank"
+        class="underline">this page</a
+      >.
+    </p>
+    <p>
+      EULA: For the End user license agreement (EULA), please refer to <a
+        href="https://paulplay.studio/eula"
+        target="_blank"
+        class="underline">this page</a
+      >.
+    </p>
+    <br /><br />
+    <h1 class="font-bold text-2xl my-2">Privacy Policy</h1>
+    <p>
+      This service is provided by PaulPlay at no cost and is intended for use as is. This page is used to inform
+      visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided
+      to use our Service. If you choose to use this Service, then you agree to the collection and use of information in
+      relation to this policy. The Personal Information that we collect is used for providing and improving the Service.
+      We will not use or share your information with anyone except as described in this Privacy Policy.
+    </p>
+    <br />
+    <h2 class="font-bold text-xl">What data we collect</h2>
+    <p>
+      Our partners (e.g. Google or Stripe) collect details of which version of web browser you used and information on
+      how you use the site using page tagging techniques. If you provide your consent, we use Google Analytics and our
+      own cookies to collect information about how you use AutoGuessr. For a better experience, while using our Service,
+      we may require you to provide us with certain personally identifiable information such as a username. If you
+      create an account we will also store account-specific information such as your level or experience. By creating an
+      account you accept that we may share non-sensitive information (like your username, email address, experience or
+      other associated data) with other users.
+      <br /> <br />
+      Google Analytics processes information about:
+      <br />
+      - the pages you visit on AutoGuessr
+      <br />
+      - how long you spend on each page
+      <br />
+      - how you got to the site
+      <br />
+      - what you click on while you're visiting the site
+      <br /> <br />
+      We make sure you cannot be directly identified by Google Analytics data. We do this by using Google Analytics' IP address
+      anonymisation feature. We will not combine analytics information with other data sets in a way that would directly
+      identify who you are.
+      <br /><br />
+      If you register an account on our website, we will collect your username, IP address (to keep multiplayer secure and
+      prevent cheaters) and email address that you have provided as well as other account related details that you provide
+      at your own choice. Additionally, we save game-related information such as, but not limited to, your account level.
+      <br /><br />
+      If you purchase any item in the game/on the site, the data you provide might be stored and processed using stripe.
+      Please refer to stripe's
+      <a href="https://stripe.com/privacy" target="_blank" class="underline">privacy policy</a> for more information.
+    </p>
+    <br />
+    <h2 class="font-bold text-xl">How we use your data</h2>
+    <p>
+      The data we collect using Google Analytics is transferred and stored with Google where we analyse it with Google
+      Analytics software (Google Analytics 4). We also use your data, such as your username, in our multiplayer
+      experiences and may share that with other players.
+    </p>
+    <br />
+    <h2 class="font-bold text-xl">Why we collect data</h2>
+    <p>
+      We use the information we collect through the explained means to see how you use AutoGuessr and to see how well
+      the site performs on your device. We also strive to improve our website by analysing issues. Furthermore, we use
+      your data to enrich multiplayer functionalities.
+    </p>
+    <br /> <br />
+    <h2 class="font-bold text-xl">How long we keep your data</h2>
+    <p>
+      We will only retain your personal data for as long as it is needed for the purposes set out in this document or
+      for as long as the law requires us to.
+    </p>
+    <br /><br />
+    <h2 class="font-bold text-xl">How we keep your data secure</h2>
+    <p>
+      We are committed to doing all that we can to keep your data secure. We set up systems and processes to prevent
+      unauthorised access to or disclosure of the data we collect about you. For example, we protect your data using
+      varying levels of encryption. All third parties that process personal data for AutoGuessr are required to keep
+      that data secure. We also only store a hash of your password if you sign up.
+    </p>
+    <br /><br />
+    <h2 class="font-bold text-xl">Changes to this policy</h2>
+    <p>
+      We may change this privacy notice. When we make changes to this notice, the 'last updated' date below will also
+      change. Any changes to this privacy notice will apply to you and your data immediately. If these changes affect
+      how your personal data is processed, AutoGuessr will take reasonable steps to make sure you know.
+      <br />
+      Last updated: 24 April 2024
+    </p>
+    <br /><br />
+    <h2 class="font-bold text-xl">Contact</h2>
+    <p>
+      If you have any questions regarding this policy, do not hesitate to contact us via the
+      <a href="https://paulplay.studio/legal" target="_blank" class="underline">contact form</a>
+      on the bottom of this page.
+    </p>
+  </div>
+</div>
