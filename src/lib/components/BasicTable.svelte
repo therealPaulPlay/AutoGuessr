2f6a5ac475c08fb1d28ffe887dc6b13c6064d1a9
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2025-03-14 16:08:47 +0200

    fix: handle empty arrays for BasicTable by displaying a message

--- src/lib/components/BasicTable.svelte
+++ src/lib/components/BasicTable.svelte
@@ -1,11 +1,17 @@
 <script>
-	let { array } = $props();
+	let { array, emptyMessage } = $props();
 </script>
 
 <div class="w-full bg-tanLight rounded-lg px-3 overflow-scroll max-h-32 overflow-x-hidden">
-	{#each array as ele}
-        <div class="w-full py-1 text-center text-black truncate border-b-2 border-black/10">
-            {ele}
-        </div>
-    {/each}
+	{#if array.length > 0}
+		{#each array as ele}
+			<div class="w-full py-1 text-center text-black truncate border-b-2 border-black/10">
+				{ele}
+			</div>
+		{/each}
+	{:else}
+		<div class="w-full py-1 text-center text-black truncate border-b-2 border-black/10">
+			{emptyMessage}
+		</div>
+	{/if}
 </div>
