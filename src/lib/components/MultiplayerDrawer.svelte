1f2f3fd9f7360ce6e4b0c8639006f1375ba585aa
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2025-03-30 14:54:50 +0200

    feat: new design for mulitplayer drawer

--- src/lib/components/MultiplayerDrawer.svelte
+++ src/lib/components/MultiplayerDrawer.svelte
@@ -5,6 +5,7 @@
 	import { gsap } from "gsap";
 	import Flip from "gsap/dist/Flip";
 	import { getPlayerInfo, isPlayerInGame } from "$lib/utils/multiplayer";
+	import Button from "./Button.svelte";
 	gsap.registerPlugin(Flip);
 
 	let multiplayerDrawerFlag = $state(false);
@@ -13,7 +14,7 @@
 	function handleDrawerToggle() {
 		const drawerState = Flip.getState(".drawer");
 
-		const rightValue = !multiplayerDrawerFlag ? "1rem" : "-98%";
+		const rightValue = !multiplayerDrawerFlag ? "1rem" : "-100%";
 		drawerElement.style.right = rightValue;
 
 		multiplayerDrawerFlag = !multiplayerDrawerFlag;
@@ -32,27 +33,27 @@
 </script>
 
 <div
-	class="bg-defaultShadow relative w-full h-full p-1.5 rounded-lg drawer"
-	style="right: -98%"
+	class="bg-defaultShadow relative w-full h-full p-2.5 rounded-lg drawer"
+	style="right: -100%"
 	bind:this={drawerElement}
 >
-	<button
-		class="absolute top-1/2 -translate-y-1/2 -z-10 rounded-l-lg -left-9 bg-defaultShadow px-1.5 py-12"
-		onclick={handleDrawerToggle}
-		><div class="arrow-icon">
-			<ChevronsLeft strokeWidth={3} color="var(--white)" />
-		</div></button
-	>
+	<div class="absolute top-1/2 -translate-y-1/2 -z-10 rounded-l-2xl left-0 -translate-x-full bg-defaultShadow p-2.5">
+		<Button buttonHeight="7rem" buttonWidth="3rem" shadowHeight="0.33rem" onclick={handleDrawerToggle}>
+			<div class="arrow-icon">
+				<ChevronsLeft strokeWidth={3} size={30} color="var(--white)" />
+			</div>
+		</Button>
+	</div>
 	<div class="w-full h-full">
-		<div class="bg-tanLight rounded text-center mb-5 p-2">
-			<span class="text-2xl text-orange font-bold">Players</span>
+		<div class="bg-tanLight rounded text-center mb-2.5 p-2">
+			<span class="text-2xl text-orange font-semibold">Players</span>
 		</div>
-		<div class="bg-tanLight w-full max-h-96 rounded px-2 overflow-x-hidden overflow-y-auto no-last-border">
+		<div class="bg-white w-full max-h-96 rounded px-2 overflow-x-hidden overflow-y-auto no-last-border">
 			{#each $currentPlayers as element (element)}
 				<!-- Flip animation works in Firefox but not in chrome for some reason. Maybe try GSAP? -->
 				<div
 					animate:flip={{}}
-					class="w-full md:min-w-64 md:max-w-80 flex justify-between p-1 text-center text-2xl text-black truncate border-b-2 border-black/10"
+					class="w-full md:min-w-64 md:max-w-80 flex justify-between p-2.5 text-center text-base text-black truncate border-b-[3px] border-tanDark"
 					style:font-weight={element.id == $peer.id ? 600 : ""}
 				>
 					<p class="overflow-clip text-ellipsis px-4 w-5/6">
