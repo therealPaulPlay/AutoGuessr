7633fab3557b3e504ddc8d723efb99a3157d0516
Amr-Yousef <3mr.g4mer.channel@gmail.com>
2025-03-30 14:53:52 +0200

    fix: fix endscreen flex issue when multiplayer table is long

--- src/routes/game/end/+page.svelte
+++ src/routes/game/end/+page.svelte
@@ -19,6 +19,7 @@
 
 	let mainContent = $state();
 	let resultTable = $state();
+	let multiplayerTable = $state();
 	let watermark = $state();
 	let innerWidth = $state();
 	let showPlayAgain = $state(true);
@@ -94,14 +95,14 @@
 
 <svelte:window bind:innerWidth />
 
-<content class="flex items-center justify-center w-full min-h-full" bind:this={mainContent}>
-	<div class="flex justify-center items-center flex-col w-full max-w-2xl pb-5 pt-2">
+<content class="flex-fix flex-col w-full h-full" bind:this={mainContent}>
+	<div class="flex flex-fix flex-col w-full h-full overflow-y-auto max-w-2xl">
 		<h1 class="text-7xl max-md:text-5xl font-bold text-wrap text-orange mb-6 text-center">{title}</h1>
 		<div class="mb-12 text-center w-4/5 text-base">
 			<div class="rounded-xl bg-tanMedium w-fit px-2 mx-auto mb-2 no-capture">
 				<p>Tip: {$score < 5 ? "Get 5 correct guesses to draw a card!" : "Higher scores result in more rare cards!"}</p>
 			</div>
-			<p class="w-full max-w-full">
+			<p class="w-full">
 				You got <span class="text-green font-bold">{$score}</span>
 				correct guess{$score == 1 ? "" : "es"}!
 				{#if $score == $highscore}
@@ -114,31 +115,32 @@
 		</div>
 		{#if $multiplayerFlag}
 			<div
-				class="w-4/5 bg-tanMedium rounded-lg px-2 mb-2 -mt-10 max-h-[6.5rem] overflow-y-auto no-capture no-last-border"
+				bind:this={multiplayerTable}
+				class="w-4/5 mb-10 bg-white rounded-lg overflow-auto overscroll-none drop-shadow-[0px_5px_0px_var(--white-shadow)] no-scrollbar min-h-40 max-h-[30vh]"
 			>
-				{#each $currentPlayers as element (element)}
-					<!-- Flip animation works in Firefox but not in chrome for some reason. Maybe try GSAP? -->
-					<div
-						animate:flip={{}}
-						class="w-full flex justify-between p-1 text-center text-black truncate border-b-2 border-black/10"
-						style:font-weight={element.id == $peer.id ? 600 : ""}
-					>
-						<p class="w-5/6 overflow-clip text-ellipsis flex justify-center items-center">
-							{getPlayerInfo(element.id).name}
-							{#if !isPlayerInGame(element.id)}
-								&nbsp;<Skull strokeWidth={2.5} size={16} color={"var(--black)"} />
-							{/if}
-						</p>
-						<p class="w-1/6">
-							{element.score}
-						</p>
-					</div>
-				{/each}
+				<table class="table-auto w-full h-full">
+					<thead class="text-white bg-orange border-b-2 border-white">
+						<tr class="head-row">
+							<th>Player</th>
+							<th>Rounds survived</th>
+							<th>Score</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each $currentPlayers as player}
+							<tr class="text-center row bg-tanDark border-b-[3px] border-tanDark">
+								<td>{player.name}</td>
+								<td>{player.round}</td>
+								<td>{player.score}</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
 			</div>
 		{/if}
 		<div
 			bind:this={resultTable}
-			class="w-4/5 mb-10 bg-white rounded-lg overflow-auto overscroll-none drop-shadow-[0px_5px_0px_var(--white-shadow)] no-scrollbar max-h-[30vh]"
+			class="w-4/5 mb-10 bg-white rounded-lg overflow-auto overscroll-none drop-shadow-[0px_5px_0px_var(--white-shadow)] no-scrollbar min-h-40 max-h-[30vh]"
 		>
 			<table class="table-auto w-full h-full">
 				<thead class="text-white bg-orange border-b-2 border-white">
@@ -152,7 +154,7 @@
 				</thead>
 				<tbody>
 					{#each $gameRounds as round, index}
-						<tr class="text-center row" class:green-row={round.rewardFlag} class:red-row={round.penaltyFlag}>
+						<tr class="text-center row border-b-[3px] border-tanDark" class:green-row={round.rewardFlag} class:red-row={round.penaltyFlag}>
 							<td>{index + 1}</td>
 							<td>${round.guess.toLocaleString()}</td>
 							<td>${round.answer.toLocaleString()}</td>
@@ -166,67 +168,67 @@
 		<p bind:this={watermark} class="text-base hidden">
 			Play on<span class="text-orange font-semibold">&nbsp;AutoGuessr.com!</span>
 		</p>
-		<div class="flex gap-5 flex-wrap justify-center items-center no-capture">
-			<!-- TODO: The peer.isHost is not reactive, maybe put into an effect (?) -->
-			<Button
-				buttonHeight="4rem"
-				buttonWidth={innerWidth >= 768 ? "14rem" : "4rem"}
-				shadowHeight="0.5rem"
-				color="var(--green-button)"
-				bgcolor="var(--green-button-dark)"
-				disabled={$multiplayerFlag && (!$peer?.isHost || $playersInGame?.length || $currentPlayers.length <= 1)}
-				onclick={async () => {
-					if ($multiplayerFlag && !playAgainClicked) {
-						playAgainClicked = true;
-						let startQuestions = await generateNewQuestions();
-						$peer?.updateStorage("questionsIds", startQuestions);
-						$peer?.updateStorage("gameInProgress", true);
-						$peer?.updateStorage("matchIndex", $peer?.getStorage?.matchIndex + 1);
-						resetMultiplayerScores();
-						return;
-					}
-					goto("/game");
-				}}
-			>
-				{#if innerWidth >= 768}
-					<!-- Render text on larger screens -->
-					<span class="text-white font-semibold text-3xl">
-						{#if playAgainClicked}
-							<div class="animate-spin">
-								<LoaderCircle strokeWidth={3} size={28} color={"var(--white)"} />
-							</div>
-						{:else}
-							Play again
-						{/if}
-					</span>
-				{:else}
-					<!-- Render an icon on smaller screens -->
-					<span class="text-white text-3xl">
-						{#if playAgainClicked}
-							<div class="animate-spin">
-								<LoaderCircle strokeWidth={3} size={28} color={"var(--white)"} />
-							</div>
-						{:else}
-							<Repeat strokeWidth={3} size={28} />
-						{/if}</span
-					>
-				{/if}
-			</Button>
-			<Button
-				buttonHeight="4rem"
-				buttonWidth="4rem"
-				shadowHeight="0.5rem"
-				onclick={() => {
-					if ($multiplayerFlag) leaveMultiplayerRoom();
-					goto("/");
-				}}
-			>
-				<span class="text-white text-3xl"><Home strokeWidth={3} size={28} /></span>
-			</Button>
-			<Button buttonHeight="4rem" buttonWidth="4rem" shadowHeight="0.5rem" onclick={captureScreen}>
-				<span class="text-white text-3xl"><Share strokeWidth={3} size={28} /></span>
-			</Button>
-		</div>
+	</div>
+	<div class="flex gap-5 flex-wrap justify-center items-center no-capture my-4">
+		<!-- TODO: The peer.isHost is not reactive, maybe put into an effect (?) -->
+		<Button
+			buttonHeight="4rem"
+			buttonWidth={innerWidth >= 768 ? "14rem" : "4rem"}
+			shadowHeight="0.5rem"
+			color="var(--green-button)"
+			bgcolor="var(--green-button-dark)"
+			disabled={$multiplayerFlag && (!$peer?.isHost || $playersInGame?.length || $currentPlayers.length <= 1)}
+			onclick={async () => {
+				if ($multiplayerFlag && !playAgainClicked) {
+					playAgainClicked = true;
+					let startQuestions = await generateNewQuestions();
+					$peer?.updateStorage("questionsIds", startQuestions);
+					$peer?.updateStorage("gameInProgress", true);
+					$peer?.updateStorage("matchIndex", $peer?.getStorage?.matchIndex + 1);
+					resetMultiplayerScores();
+					return;
+				}
+				goto("/game");
+			}}
+		>
+			{#if innerWidth >= 768}
+				<!-- Render text on larger screens -->
+				<span class="text-white font-semibold text-3xl">
+					{#if playAgainClicked}
+						<div class="animate-spin">
+							<LoaderCircle strokeWidth={3} size={28} color={"var(--white)"} />
+						</div>
+					{:else}
+						Play again
+					{/if}
+				</span>
+			{:else}
+				<!-- Render an icon on smaller screens -->
+				<span class="text-white text-3xl">
+					{#if playAgainClicked}
+						<div class="animate-spin">
+							<LoaderCircle strokeWidth={3} size={28} color={"var(--white)"} />
+						</div>
+					{:else}
+						<Repeat strokeWidth={3} size={28} />
+					{/if}</span
+				>
+			{/if}
+		</Button>
+		<Button
+			buttonHeight="4rem"
+			buttonWidth="4rem"
+			shadowHeight="0.5rem"
+			onclick={() => {
+				if ($multiplayerFlag) leaveMultiplayerRoom();
+				goto("/");
+			}}
+		>
+			<span class="text-white text-3xl"><Home strokeWidth={3} size={28} /></span>
+		</Button>
+		<Button buttonHeight="4rem" buttonWidth="4rem" shadowHeight="0.5rem" onclick={captureScreen}>
+			<span class="text-white text-3xl"><Share strokeWidth={3} size={28} /></span>
+		</Button>
 	</div>
 </content>
 
@@ -247,7 +249,9 @@
 		padding: 1rem 0.5rem;
 	}
 
-	.no-last-border > div:last-child {
-		border-bottom: none !important;
+	.flex-fix {
+		display: flex;
+		align-items: safe center;
+		justify-content: safe center;
 	}
 </style>
