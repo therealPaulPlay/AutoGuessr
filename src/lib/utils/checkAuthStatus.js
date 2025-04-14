775229df8d2fcabad7b350ed4a471b96cbf2f706
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-04-14 02:51:30 +0200

    feat: switch to immediate discovery for playlight, fix token expiry function

--- src/lib/utils/checkAuthStatus.js
+++ src/lib/utils/checkAuthStatus.js
@@ -53,23 +53,24 @@ export function signOut() {
 }
 
 function isTokenExpired(token) {
-    const base64Url = token.split(".")[1];
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(
-        atob(base64)
-            .split("")
-            .map(function (c) {
-                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-            })
-            .join(""),
-    );
-
+    let expired;
     try {
+        const base64Url = token.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split("")
+                .map(function (c) {
+                    return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+                })
+                .join(""),
+        );
+
         const { exp } = JSON.parse(jsonPayload);
-        const expired = Date.now() >= exp * 1000;
-        return expired;
+        expired = Date.now() >= exp * 1000;
     } catch {
-        // If the token cannot be parsed, it is invalid and will be counted as expired
-        return false;
+        expired = true; // If the token cannot be parsed, it will be counted as expired
     }
+    if (expired) signOut();
+    return expired;
 }
